c272fa18834c712b1b2908e27b936a0c27f3d416
nocarryr <matt@nomadic-recording.com>
2016-03-14 16:52:50 -0500

    Add cli tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import os
 import string
+import threading
 
 import pytest
 
@@ -44,3 +45,81 @@ def config_handler_fixtures(tmpdir, identity_store):
         conf_files=conf_files,
         identity_store=identity_store,
     )
+
+
+@pytest.fixture
+def cli_app(config_handler_fixtures):
+    import main
+    main.identity_store = config_handler_fixtures['identity_store']
+
+    class StdOut(object):
+        def __init__(self):
+            self.clear()
+        def write(self, s):
+            self.buffer += s
+        def read(self):
+            raise NotImplementedError
+        def clear(self):
+            self.buffer = ''
+        def _read(self):
+            s = self.buffer
+            self.clear()
+            return s
+
+    class CliApp(main.Main):
+        def send_input(self, s):
+            t = self.cli_thread
+            t.input_buffer = s
+            t.input_ready.set()
+            t.postcmd.wait()
+            t.postcmd.clear()
+        def preloop(self):
+            self.cli_thread.running.set()
+        def postcmd(self, stop, line):
+            self.cli_thread.postcmd.set()
+            return stop
+        def pseudo_raw_input(self, prompt):
+            t = self.cli_thread
+            t.running.set()
+            t.input_ready.wait()
+            if isinstance(t.input_buffer, list):
+                data = t.input_buffer[0]
+                t.input_buffer = t.input_buffer[1:]
+                if not len(t.input_buffer):
+                    t.input_buffer = None
+                    t.input_ready.clear()
+            else:
+                data = t.input_buffer
+                t.input_ready.clear()
+            return data
+
+    class CliThread(threading.Thread):
+        def __init__(self):
+            super(CliThread, self).__init__()
+            self.running = threading.Event()
+            self.stopped = threading.Event()
+            self.input_ready = threading.Event()
+            self.postcmd = threading.Event()
+        def run(self):
+            app = self.cli_app = CliApp()
+            app.stdout = StdOut()
+            app.cli_thread = self
+            app._cmdloop()
+            self.running.clear()
+            self.stopped.set()
+        def stop(self):
+            if self.stopped.is_set():
+                return
+            self.cli_app.send_input('exit')
+            self.stopped.wait()
+
+    cli_thread = CliThread()
+    cli_thread.daemon = True
+    cli_thread.start()
+    cli_thread.running.wait()
+    app = cli_thread.cli_app
+    app.config_handler_fixtures = config_handler_fixtures
+
+    def fin():
+        cli_thread.stop()
+    return app
---
+++ tests/test_cli.py
@@ -0,0 +1,26 @@
+import os
+
+def test_store_empty(cli_app):
+    identity_store = cli_app.config_handler_fixtures['identity_store']
+    cli_app.send_input('save')
+    out = cli_app.stdout._read()
+    print(out)
+    assert len(list(identity_store.values())) == 1
+    identity = list(identity_store.values())[0]
+    assert identity.access_key_id is None
+    assert identity.secret_access_key is None
+
+def test_add_command(cli_app, identity_fixures):
+    from awsident.identity import Identity
+
+    identity_store = cli_app.config_handler_fixtures['identity_store']
+    data = identity_fixures[0]
+    args = ['add']
+
+    args.extend([data[key] for key in cli_app.add_command_steps])
+    cli_app.send_input(args)
+    print(cli_app.stdout._read())
+
+    identity = identity_store.get(data['access_key_id'])
+    test_identity = Identity(**data)
+    assert identity == test_identity
